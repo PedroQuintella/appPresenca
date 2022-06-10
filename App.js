324df9e0d1e6270311bda3ea7635a02f9af228a7
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-06-09 21:21:21 -0300

    Feat: Primeiras configurações

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import { NavigationContainer, StackActions } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Usuarios from "./src/pages/Usuarios";
+import AlunoLogin from "./src/pages/AlunoLogin";
+import ProfessorLogin from "./src/pages/ProfessorLogin";
+import TurmasAluno from "./src/pages/TurmasAluno";
+import TurmasProfessor from "./src/pages/TurmasProfessor";
+import AlunosTurma from "./src/pages/AlunosTurma";
+import DisciplinasTurma from "./src/pages/DisciplinasTurma";
+
+const stack = createNativeStackNavigator();
+
+export default function App(){
+  return(
+    <NavigationContainer>
+      <stack.Navigator>
+
+        <stack.Screen name="Usuarios" component={Usuarios} />
+        <stack.Screen name="AlunoLogin" component={AlunoLogin} />
+        <stack.Screen name="ProfessorLogin" component={ProfessorLogin} />
+        <stack.Screen name="TurmasAluno" component={TurmasAluno} />
+        <stack.Screen name="TurmasProfessor" component={TurmasProfessor} />
+        <stack.Screen name="AlunosTurma" component={AlunosTurma} />
+        <stack.Screen name="DisciplinasTurma" component={DisciplinasTurma} />
+      </stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

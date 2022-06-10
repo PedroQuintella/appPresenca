cfd5be1124fd8dcb9e9beae255c00cc2a845e5ee
Pedro Quintella <49735582+PedroQuintella@users.noreply.github.com>
2022-06-09 21:24:00 -0300

    Feat: Página de disciplinas do aluno

---
+++ src/pages/DisciplinasTurma/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+import { useNavigation } from "@react-navigation/native";
+
+import { Button } from "react-native";
+
+export default function DisciplinasTurma(){
+    const navigation = useNavigation();
+
+    return(
+        <View style={styles.container}>
+            <Text>Suas Disciplinas:</Text>
+            <Button title="Ver Disciplinas" onPress={ () => navigation.navigate('ProfessorLogin') } />
+            <Text>Disciplina 1</Text>
+            <Button title="Ver Disciplinas" onPress={ () => navigation.navigate('ProfessorLogin') } />
+            <Text>Disciplina 2</Text>
+            <Button title="Ver Disciplinas" onPress={ () => navigation.navigate('ProfessorLogin') } />
+            <Text>Disciplina 3</Text>
+            <Button title="Ver Disciplinas" onPress={ () => navigation.navigate('ProfessorLogin') } />
+            <Text>Disciplina 4</Text>
+            <Button title="Ver Disciplinas" onPress={ () => navigation.navigate('ProfessorLogin') } />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logotipoImage:{
+        width: '1234px',
+    }
+})
